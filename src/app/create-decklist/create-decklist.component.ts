95468d4da6c8df49efcaaf30a5193fe78fd9ccb8
Mike Dacanay <migd64@gmail.com>
2017-10-02 00:26:54 -0700

    add card validator working as intended,work on adding pictures to card on hover

--- src/app/create-decklist/create-decklist.component.ts
+++ src/app/create-decklist/create-decklist.component.ts
@@ -26,12 +26,22 @@ export class CreateDecklistComponent implements OnInit {
   grabCard(){
   	// console.log(this._cards.CardCollection);
   	if(String(this.card).toLowerCase() in this._cards.CardCollection){
-  		this.decklist.push(String(this.card));
-      this.validator = true;
-  	}else{
-  		this.validator = false;
-  	}
-
-    /// Work on counting if it exist on array twice
+      let counter = 0;
+      for(let i=0; i < this.decklist.length; i++){
+        if(this.decklist[i] === this.card){
+          counter+=2
+        }     
+      }
+      if(counter<3){
+        this.decklist.push(String(this.card));
+        this.validator = true;
+        this.validator2 = true;
+      }else{
+        this.validator2 = false;
+      }
+    }else{
+      this.validator2 = true;
+      this.validator = false;
+    }
   }
 }
